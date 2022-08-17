adae3a4171d8d4a954aff79866a93f4e823f955c
daniel <dsbadal@gmail.com>
2022-08-17 10:18:20 -0700

    Expand sprite includes

--- c2ephp.php
+++ c2ephp.php
@@ -1,6 +1,7 @@
 <?php
 
 //support
+require_once(dirname(__FILE__) . '/src/Support/IReader.php' );
 require_once(dirname(__FILE__).'/src/Support/FileReader.php');
 require_once(dirname(__FILE__).'/src/Support/StringReader.php');
 require_once(dirname(__FILE__).'/src/Support/Archiver.php');
@@ -10,9 +11,14 @@ require_once(dirname(__FILE__).'/src/Agents/Pray/PRAYFile.php');
 require_once(dirname(__FILE__).'/src/Agents/COB/COB.php');
 
 //CAOS
-require_once(dirname(__FILE__).'/src/CAOS/highlight/CAOSHighlighter.php');
+//require_once(dirname(__FILE__).'/src/CAOS/highlight/CAOSHighlighter.php');
 
 //images
+require_once(dirname(__FILE__).'/src/Sprites/SpriteFile.php');
+require_once(dirname(__FILE__).'/src/Sprites/SpriteFrame.php');
 require_once(dirname(__FILE__).'/src/Sprites/C16File.php');
+require_once(dirname(__FILE__).'/src/Sprites/C16Frame.php');
 require_once(dirname(__FILE__).'/src/Sprites/S16File.php');
-require_once(dirname(__FILE__).'/src/Sprites/S16File.php');
+require_once(dirname(__FILE__).'/src/Sprites/S16Frame.php');
+require_once(dirname(__FILE__).'/src/Sprites/SPRFile.php');
+require_once(dirname(__FILE__).'/src/Sprites/SPRFrame.php');
