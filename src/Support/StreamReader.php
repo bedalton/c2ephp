41d84a8d7f8e0c883691665cde06ad776d176b1f
daniel <dsbadal@gmail.com>
2023-09-07 12:12:55 -0700

    Create StreamReader.php

---
+++ src/Support/StreamReader.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace C2ePhp\Support;
+
+use Exception;
+
+/**
+ * Wrapper class to make reading files easier.
+ * Quite often you'll have to make a FileReader in order to use other
+ * read from a file. \n
+ * This is a simple process, simply pass in the filename to FileReader()
+ */
+class StreamReader implements IReader {
+
+	const CP_DECODING_DEFAULT = FALSE;
+    /// @cond INTERNAL_DOCS
+
+    /** @var resource file handle */
+    private $handle;
+
+    private $size;
+
+    /// @endcond
+
+	/**
+	 * Instantiates a file reader for the given file, ready for reading.
+	 * @param resource $handle
+	 * @throws Exception
+	 */
+    public function __construct($handle) {
+        if (!is_resource($handle)) {
+			throw new Exception("Handle is not to valid io handle");
+		}
+        $this->size = -1;
+        $this->handle = $handle;
+    }
+
+	/**
+	 * Reads a specific number of bytes, returning as string
+	 * @param int|null $length
+	 * @param bool $cpDecode decode CP-1252 text to UTF-8
+	 * @param bool $throwing throw exception on not-enough-bytes
+	 * @return string
+	 * @throws Exception
+	 */
+    public function read(?int $length, bool $cpDecode = self::CP_DECODING_DEFAULT, bool $throwing = TRUE) {
+		if (is_null($length)) {
+			throw new Exception("Cannot read bytes with null length");
+		}
+        if ($this->size < 0 && feof($this->handle)) {
+			throw new Exception("Cannot read past end of stream. Expected: $length bytes. Found: 0");
+		} else if ($this->getPosition() + $length > $this->size) {
+            if ($throwing) {
+                $bytesPast = ($this->size - $this->getPosition());
+                throw new Exception("Cannot read past end of file. Expected: $length bytes. Found: $bytesPast");
+            }
+            return NULL;
+        }
+		$str = $length ? fread($this->handle, $length) : '';
+		if ($str && $cpDecode) {
+			$str = EncodingHelper::decodeCreaturesEncoding($str);
+		}
+        return $str;
+    }
+
+    /**
+     * @param int|null $length
+     * @return int
+     * @throws Exception
+     */
+    public function readInt(?int $length) {
+		if (is_null($length)) {
+			throw new Exception("Cannot read int with null request byte length");
+		}
+        $int = 0;
+		if ($this->size < 0 && feof($this->handle)) {
+			throw new Exception("Cannot read past end of stream. Expected: $length bytes. Found: 0");
+		} else if (($this->getPosition() + $length) > $this->size) {
+            throw new Exception("Cannot read int($length) past end of buffer");
+        }
+        for ($x = 0; $x < $length; $x++) {
+            $buffer = fgetc($this->handle);
+            if ($buffer === FALSE)
+                throw new Exception("Cannot read int($length); Read failure");
+            $buffer = (ord($buffer) << ($x * 8));
+            $int += $buffer;
+        }
+        return $int;
+    }
+
+    /**
+     * Gets current read position in file stream
+     * @return false|int
+     */
+    public function getPosition() {
+        return ftell($this->handle);
+    }
+
+    /**
+     * Check if there is more data at current position in buffer
+     * @return bool <b>true</b> if there is more data to read at current buffer position
+     */
+    public function hasNext() {
+        return $this->getPosition() < $this->size;
+    }
+
+    /**
+     * Reads a section of the file stream from a given point
+     * @param int|null $start
+     * @param int|null $length
+     * @return false|string
+     * @throws Exception
+     */
+    public function getSubString(?int $start, ?int $length = NULL) {
+		if (is_null($start)) {
+			throw new Exception("Cannot get substring with null start position");
+		}
+        $oldPosition = ftell($this->handle);
+        fseek($this->handle, $start);
+        if (is_null($length)) {
+            $length = $this->size - $start;
+        }
+        $data = fread($this->handle, $length);
+        fseek($this->handle, $oldPosition);
+        return $data;
+    }
+
+    /**
+	 * @return string
+     * @throws Exception
+     */
+    public function readCString() {
+        $string = '';
+        while (!is_null($char = $this->read(1, self::CP_DECODING_DEFAULT, FALSE))) {
+            if (ord($char) === 0) {
+                break;
+            }
+            $string .= $char;
+        }
+        return $string;
+    }
+
+	/**
+	 * Changes the current position in the reader's stream
+	 *
+	 * This is analogous to fseek in C or PHP.
+	 * @param int|null $position
+	 * @return void
+	 * @throws Exception
+	 */
+    public function seek(?int $position) {
+		if (is_null($position)) {
+			throw new Exception("Cannot seek to null position");
+		}
+        fseek($this->handle, $position);
+    }
+
+	/**
+	 * Advances the position of the reader by $count.
+	 *
+	 * @param int|null $count
+	 * @return void
+	 * @throws Exception
+	 */
+    public function skip(?int $count) {
+		if (is_null($count)) {
+			throw new Exception("Cannot skip bytes with null length");
+		}
+        fseek($this->handle, $count, SEEK_CUR);
+    }
+
+    public function close() {
+        if (is_resource($this->handle)) {
+			fclose($this->handle);
+		}
+    }
+}
+
+
